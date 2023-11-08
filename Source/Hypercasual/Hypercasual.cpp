e95544d04b3e747c1f6bc69004e22057f7cb7036
Hqrrg <hqrryhowes@gmail.com>
2023-11-08 17:19:00 +0000

    Initial commit

---
+++ Source/Hypercasual/Hypercasual.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Hypercasual.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Hypercasual, "Hypercasual" );
+ 
---
+++ Source/Hypercasual/HypercasualGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HypercasualGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AHypercasualGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AHypercasualGameMode();
+};
+
+
+
